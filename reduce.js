939c0d496b0816fe1ccfec084b3d3c23de466272
Menashe <mborukhov770@gmail.com>
2021-08-12 18:47:09 -0600

    second complete

--- reduce.js
+++ reduce.js
@@ -24,7 +24,20 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+    let arrayed = str.toLowerCase().split('');
+    const vowels = 'aeiou';
+    return arrayed.reduce((acc, x) => {
+        if (vowels.includes(x)) {
+            if (acc[x]) {
+                acc[x]++;
+            } else {
+                acc[x] = 1;
+            }
+        }
+        return acc;
+    }, {})
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
